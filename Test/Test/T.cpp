e0da37ad676a8bf9cc216518195ee2a6d2391c92
csllove <895395113@qq.com>
2019-08-31 10:33:09 +0800

    2019 8 31 version

--- Test/Test/T.cpp
+++ Test/Test/T.cpp
@@ -5,46 +5,10 @@
 #include <stdio.h>
 using namespace std;
 
-void cass(int a[5]);
-
 int main()
 {
-	//int column[6] = { 0 };
-	//int b,c,d,e;
-	//float f = 1;
-	///*c = scanf("%d%d",&b,&d);
-	//e = printf("%d\n",2);
-	//cout << e << endl;*/
-	//printf("%f\n",f);
-	//cass(column);
-
-
-	puts("abcd");
-	puts("I am a good boy！");
-	int a[10] = {};
-	for (int i = 0; i < 10; i++)
-		printf("%d\n",a[i]);
-
-
-
-
-
-#if 0 
-	char *a = "abcd";//采用指针存储字符串,其实质就是把字符串的首地值附给基类型为char的指针变量,从而可以从字符串首元素开始对字符串进行操作
-	cout << strlen(a) << endl;
-	cout << sizeof(a) << endl;
-
-
-#endif
-
+	char  ch;
+	cout << "Hello World" << endl;
+	system("pause");
 	return 0;
-
-}
-
-void cass(int a[5])
-{
-	for (int i = 0; i < 6; i++)
-	{
-		printf("%d\n",a[i]);
-	}
 }
