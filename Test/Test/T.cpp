7b7c57c3bacf000f0776dbbe955f98da81e85ee4
csllove <895395113@qq.com>
2019-09-02 21:29:42 +0800

    It's a time.h judge version

--- Test/Test/T.cpp
+++ Test/Test/T.cpp
@@ -1,14 +1,23 @@
+
+
 #include <iostream>
 #include <string>
-#include <cmath>
-#include<stdlib.h>
-#include <stdio.h>
+#include <stdlib.h>
+//#include <windows.h>
+#include<conio.h>
+#include <regex>     //ÕıÔò¿â
+#include<mmsystem.h> //ÒôÀÖ¿â
+#pragma  comment(lib,"winmm.lib")  //¾²Ì¬°ü
 using namespace std;
 
 int main()
 {
-	char  ch;
-	cout << "Hello World" << endl;
+	system("title Csl");
+	system("color f4");
+	system("mode con: cols=45 lines=10");
+	time_t timer = time(NULL);
+	cout << "c++time is %s" << ctime(&timer) << endl;
+	cout << clock() << endl;;
 	system("pause");
 	return 0;
 }
