2f2f12e0c471982250b1e110abe030d4198d63b4
csllove <895395113@qq.com>
2019-09-08 11:22:54 +0800

    Long way test

--- Test/Test/T.cpp
+++ Test/Test/T.cpp
@@ -3,21 +3,15 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-//#include <windows.h>
-#include<conio.h>
-#include <regex>     //ÕıÔò¿â
-#include<mmsystem.h> //ÒôÀÖ¿â
-#pragma  comment(lib,"winmm.lib")  //¾²Ì¬°ü
 using namespace std;
 
 int main()
 {
-	system("title Csl");
-	system("color f4");
-	system("mode con: cols=45 lines=10");
-	time_t timer = time(NULL);
-	cout << "c++time is %s" << ctime(&timer) << endl;
-	cout << clock() << endl;;
+	char* name;
+	name = "csl";
+	cout << name;
+	cout << endl;
+	cout << "hello World!" << endl;
 	system("pause");
 	return 0;
 }
